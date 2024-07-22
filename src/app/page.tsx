124f6727fbf6a59dc5d2a714dd291292ba482b1d
Le Nguyen <lenguyen18072003@gmail.com>
2024-07-22 23:37:22 +0700

    update some misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
     <div className="flex w-full flex-col gap-y-10">
       <section className="flex flex-col items-center gap-y-7">
         <Image
-            src="https://avatars.githubusercontent.com/u/91407657?s=400&u=867ee350e32508c57cc667070adb154ca1e01955&v=4"
+            src="https://avatars.githubusercontent.com/u/91407657"
             alt="Profile photo"
             style={{ width: "auto", height: "150px" }}
             sizes="100vw"
             width={0}
             height={0}
-            className="rounded-full"
+            className="rounded-none"
         />
         <div className="flex flex-col items-center gap-y-4">
           <h1 className="text-3xl font-bold">Le Nguyen 🍉</h1>
